a300b137c490541761f518c798fb0e9a4fc06a3d
aleongit <aleix.leon@gmail.com>
2024-01-11 17:29:42 +0100

    feat: test toolbar and menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,34 @@ import { AppComponent } from './app.component';
 /*components*/
 import { CardComponent } from './card/card.component';
 import { CardMediaComponent } from './card-media/card-media.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MenuComponent } from './menu/menu.component';
 
 /*material*/
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, CardMediaComponent],
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardMediaComponent,
+    ToolbarComponent,
+    MenuComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
