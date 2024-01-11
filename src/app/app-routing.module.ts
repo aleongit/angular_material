dc528ec32ce65774a0c9bb34db55a14f9f1b1365
aleongit <aleix.leon@gmail.com>
2024-01-11 17:30:31 +0100

    feat: add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { CardMediaComponent } from './card-media/card-media.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'card', component: CardComponent },
+  { path: 'card-media', component: CardMediaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
