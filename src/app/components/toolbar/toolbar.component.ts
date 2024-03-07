cdd02a14fa9354c9e84effc4448b2c14890c020a
aleongit <aleix.leon@gmail.com>
2024-03-07 14:35:53 +0100

    refactor: toolbar material

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDividerModule],
 })
 export class ToolbarComponent {}
