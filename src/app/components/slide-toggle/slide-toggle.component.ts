80157496fa1d85739bfb02427c21a9e681de64b7
aleongit <aleix.leon@gmail.com>
2024-03-08 15:57:33 +0100

    feat: new slide toggles

--- src/app/components/slide-toggle/slide-toggle.component.ts
+++ src/app/components/slide-toggle/slide-toggle.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ThemePalette } from '@angular/material/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {
+  MatSlideToggleModule,
+  _MatSlideToggleRequiredValidatorModule,
+} from '@angular/material/slide-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-slide-toggle',
   templateUrl: './slide-toggle.component.html',
   styleUrls: ['./slide-toggle.component.scss'],
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [
+    MatSlideToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatButtonModule,
+  ],
 })
-export class SlideToggleComponent {}
+export class SlideToggleComponent {
+  /* for Configurable slide-toggle */
+  color: ThemePalette = 'accent';
+  checked = false;
+  disabled = false;
+
+  /* for Slide-toggle with forms */
+  isChecked = true;
+  formGroup = this._formBuilder.group({
+    enableWifi: '',
+    acceptTerms: ['', Validators.requiredTrue],
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  alertFormValues(formGroup: FormGroup) {
+    alert(JSON.stringify(formGroup.value, null, 2));
+  }
+}
