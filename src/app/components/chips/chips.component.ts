0f15ea4f560358ec29942105e434f7c5cd941612
aleongit <aleix.leon@gmail.com>
2024-03-08 15:32:05 +0100

    feat: add new chips

--- src/app/components/chips/chips.component.ts
+++ src/app/components/chips/chips.component.ts
@@ -5,7 +5,11 @@ import {
   MatAutocompleteSelectedEvent,
   MatAutocompleteModule,
 } from '@angular/material/autocomplete';
-import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import {
+  MatChipsModule,
+  MatChipInputEvent,
+  MatChipEditedEvent,
+} from '@angular/material/chips';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { NgFor, AsyncPipe } from '@angular/common';
@@ -18,16 +22,22 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
-
-import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
+//for stacked chips
 export interface ChipColor {
   name: string;
   color: ThemePalette;
 }
 
+//for Chips Drag and Drop
 export interface Vegetable {
+  name: string;
+}
+
+//for Chips with input
+export interface Fruit {
   name: string;
 }
 
@@ -38,19 +48,20 @@ export interface Vegetable {
   standalone: true /* component que NO depent de app.module */,
   imports: [
     MatChipsModule,
-    MatDividerModule,
     CdkDropList,
     CdkDrag,
     FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     NgFor,
+    AsyncPipe,
     MatIconModule,
     MatAutocompleteModule,
-    ReactiveFormsModule,
-    AsyncPipe,
+    MatButtonModule,
   ],
 })
 export class ChipsComponent {
+  //for Stacked Chips
   availableColors: ChipColor[] = [
     { name: 'none', color: undefined },
     { name: 'Primary', color: 'primary' },
@@ -58,6 +69,7 @@ export class ChipsComponent {
     { name: 'Warn', color: 'warn' },
   ];
 
+  //for Chips Drag and Drop
   vegetables: Vegetable[] = [
     { name: 'apple' },
     { name: 'banana' },
@@ -67,17 +79,32 @@ export class ChipsComponent {
     { name: 'cherry' },
   ];
 
+  /* for Chips Autocomplete */
   separatorKeysCodes: number[] = [ENTER, COMMA];
   fruitCtrl = new FormControl('');
   filteredFruits: Observable<string[]>;
   fruits: string[] = ['Lemon'];
   allFruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
-
   @ViewChild('fruitInput') fruitInput!: ElementRef<HTMLInputElement>;
-
   announcer = inject(LiveAnnouncer);
 
+  /* for Chips with form control */
+  keywords = ['angular', 'how-to', 'tutorial', 'accessibility'];
+  formControl = new FormControl(['angular']);
+  announcerFormControl = inject(LiveAnnouncer);
+
+  /* for Chips with input */
+  addOnBlur = true;
+  readonly separatorKeysCodesInput = [ENTER, COMMA] as const;
+  fruitsInput: Fruit[] = [
+    { name: 'Lemon' },
+    { name: 'Lime' },
+    { name: 'Apple' },
+  ];
+  announcerInput = inject(LiveAnnouncer);
+
   constructor() {
+    /* for Chips Autocomplete */
     this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
       startWith(null),
       map((fruit: string | null) =>
@@ -91,7 +118,7 @@ export class ChipsComponent {
     moveItemInArray(this.vegetables, event.previousIndex, event.currentIndex);
   }
 
-  /* Chips Autocomplete */
+  /* for Chips Autocomplete */
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
 
@@ -102,7 +129,6 @@ export class ChipsComponent {
 
     // Clear the input value
     event.chipInput!.clear();
-
     this.fruitCtrl.setValue(null);
   }
 
@@ -111,7 +137,6 @@ export class ChipsComponent {
 
     if (index >= 0) {
       this.fruits.splice(index, 1);
-
       this.announcer.announce(`Removed ${fruit}`);
     }
   }
@@ -128,5 +153,65 @@ export class ChipsComponent {
     return this.allFruits.filter((fruit) =>
       fruit.toLowerCase().includes(filterValue)
     );
+  }
+
+  /* for Chips with form control */
+  removeKeyword(keyword: string) {
+    const index = this.keywords.indexOf(keyword);
+    if (index >= 0) {
+      this.keywords.splice(index, 1);
+      this.announcerFormControl.announce(`removed ${keyword}`);
+    }
+  }
+
+  addFormControl(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our keyword
+    if (value) {
+      this.keywords.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  /* for Chips with input */
+  addInput(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruitsInput.push({ name: value });
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  removeInput(fruit: Fruit): void {
+    const index = this.fruitsInput.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruitsInput.splice(index, 1);
+
+      this.announcerInput.announce(`Removed ${fruit}`);
+    }
+  }
+
+  editInput(fruit: Fruit, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove fruit if it no longer has a name
+    if (!value) {
+      this.removeInput(fruit);
+      return;
+    }
+
+    // Edit existing fruit
+    const index = this.fruitsInput.indexOf(fruit);
+    if (index >= 0) {
+      this.fruitsInput[index].name = value;
+    }
   }
 }
