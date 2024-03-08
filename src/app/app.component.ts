f27bfd2f992c18d3325fee6e9e900aed5d4d17b6
aleongit <aleix.leon@gmail.com>
2024-03-08 15:31:32 +0100

    feat: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './shared/navigation/navigation.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, FooterComponent],
   providers: [],
 })
 export class AppComponent {
