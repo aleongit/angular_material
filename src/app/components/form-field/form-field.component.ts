2d23db4f8f472bdde4516af112910538e38f5c66
aleongit <aleix.leon@gmail.com>
2024-04-19 16:37:46 +0200

    feat: form field appearance variants

--- src/app/components/form-field/form-field.component.ts
+++ src/app/components/form-field/form-field.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-form-field',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule],
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.scss',
 })
