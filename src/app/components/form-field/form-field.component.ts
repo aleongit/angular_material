afd65b919aab066cf09d1d6b8b6c1359ad42f188
aleongit <aleix.leon@gmail.com>
2024-04-19 15:27:08 +0200

    wip: simple form field

--- src/app/components/form-field/form-field.component.ts
+++ src/app/components/form-field/form-field.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @Component({
   selector: 'app-form-field',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './form-field.component.html',
-  styleUrl: './form-field.component.scss'
+  styleUrl: './form-field.component.scss',
 })
-export class FormFieldComponent {
-
-}
+export class FormFieldComponent {}
